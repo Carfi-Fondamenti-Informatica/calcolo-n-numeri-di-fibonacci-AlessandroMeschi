c71899dbc86e25229fde8ddb4b5ccfb7c67fc5c1
AlessandroMeschi <91841733+AlessandroMeschi@users.noreply.github.com>
2021-11-12 23:15:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+   
+   int n=0, n1=1, n2=1, n3;
+   cin >> n;
+
+   if(n>=2) {
+      cout << "1" <<endl;
+      cout << "1" <<endl;
+      for(int i=2; i<n; i++) {
+         
+         n3 = n1 + n2;
+         n1 = n2;
+         n2 = n3;
+         cout << n3 << endl;
+        }
+      
+    }else{
+      
+        cout << "errore" <<endl;
+    }
    return 0;
 }
